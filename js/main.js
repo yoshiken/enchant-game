54c9068822d562a1650e6729e442b6c500edc5b3
Yoshiken <yskn.beer@gmail.com>
2017-03-15 03:05:31 +0900

    Provisional completion

--- js/main.js
+++ js/main.js
@@ -1,43 +1,78 @@
 enchant(); // enchant.jsの有効化
 
-var exist = 0;
+var shot_frames = 0;
 
 window.onload = function() {
-    var game = new Core(960, 640);
-    var sceneGameMain = new Scene();
+    game = new Core(960, 640);
+    sceneGameMain = new Scene();
     game.preload("./img/human.png");
+    game.preload("./img/pet_man.png");
     game.fps = 30;
 
-
     game.onload = function() {
-        var sprite = new Sprite(60, 60);
+
+        sprite = new Sprite(60, 60);
         sprite.x = 0;
         sprite.y = 0;
         sprite.frame = 0;
-        sprite.image = game.assets["./img/human.png"]
+        sprite.image = game.assets["./img/human.png"];
         game.rootScene.addChild(sprite);
 
-        var ShikakuCreate = function() {
+        var enemy_count = 0;
+        var enemy_index = [];
 
-        };
+        function enemy(y){
+
+            enemy_cha = new Sprite(55,57);
+            enemy_cha.frame = 0;
+            enemy_cha.scaleX = -1;
+            enemy_cha.image = game.assets["./img/pet_man.png"];
+            enemy_cha.x = 900;
+            enemy_cha.y = y*56;
+            game.rootScene.addChild(enemy_cha);
+            enemy_cha.tl.moveBy(-960,0,180);
+            enemy_index [enemy_count] = enemy_cha;
+            enemy_count += 1;
+
+        }
 
         var num = 1;
         var count = 0;
+        var sonzai = 0;
 
-        game.keybind(32, 'a');
+        game.keybind(32, 'sp');
+        game.keybind(69, 'e');
+
+        var shikaku_count = 0;
+        var shikaku_index = [];
+        score_count = 0;
+
+        score_text = new Label("Score:");
+        game.rootScene.addChild(score_text);
+
+        score = new Label(score_count);
+        score.text = score_count;
+        score.x = 70;
+        game.rootScene.addChild(score);
+
+        end = 0;
 
         sprite.addEventListener("enterframe", function() {
-          exist = 0;
-            if (game.input.a) {
-                var shikaku = new Sprite(100, 10);
-                shikaku.frame = 0;
-                shikaku.backgroundColor = "rgba(200, 255, 200, 0.5)";
-                game.rootScene.addChild(shikaku);
-                shikaku.x = sprite.x + 60;
-                shikaku.y = sprite.y + 25;
-                exist = 1;
-
+            if (shot_frames >= 10){
+                shot_frames = 0;
+                if (game.input.sp) {
+                    shikaku = new Sprite(80, 10);
+                    shikaku.frame = 0;
+                    shikaku.backgroundColor = "rgba(200, 255, 200, 1)";
+                    game.rootScene.addChild(shikaku);
+                    shikaku.x = sprite.x + 60;
+                    shikaku.y = sprite.y + 25;
+                    shikaku.tl.moveBy(900,0,45);
+                    shikaku_index[shikaku_count] = shikaku;
+                    shikaku_count += 1;
+                }
             }
+
             if (game.input.up) {
                 this.y -= 6;
             }
@@ -56,20 +91,53 @@ window.onload = function() {
             if (this.x < 0) {
                 this.x = 0
             }
-            if (count == 1) {
-                count = 0
-                sprite.frame += num;
-            }
-            count++;
-            sprite.frame += num;
+            if (count % 25 == 0){
+                var rand = Math.floor( Math.random() * 11 ) ;
+                enemy(rand);
 
-            if (exist == 1) {
-                console.log(shikaku.x);
-          }
+            }
+
+            if (shot_frames > 50){
+                shot_frames = 0;
+            }
+
+            if (enemy_count >= 1){
+                for(var i=0;i<enemy_index.length;i++){
+                    if(shikaku_count >= 1){
+                        for(var j=0;j<shikaku_index.length;j++){
+                            if(shikaku_index[j].intersect(enemy_index[i])) {
+                                game.rootScene.removeChild(score);
+                                score_count ++;
+                                score = new Label(score_count);
+                                score.text = score_count;
+                                score.x = 70;
+                                game.rootScene.addChild(score);
+
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            if (enemy_count >= 1){
+                for(var i=0;i<enemy_index.length;i++){
+                    if(sprite.intersect(enemy_index[i])) {
+                        end = 1;
+                    }
+                }
+            }        
+
+            if (end == 1){
+                alert("Score:" + score_count);
+                location.reload();
+            }
+
+            count++;
+            shot_frames += 1 ;
+            sprite.frame += num;
 
         });
     }
-
     game.start();
-
 };
